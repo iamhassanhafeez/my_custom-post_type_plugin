89a0304028fcbd95b13ceb557ed2a577a3fcbefa
Hassan Hafeez <iamhassanhafeez@gmail.com>
2025-03-23 15:59:32 +0500

    Plugin title tags setup

---
+++ custom_post_type_plugin.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: My Custom Post Type Plugin
+ * Description: This plugin creates custom post type "Books".
+ * Plugin URI: mailto:iamhassanhafeez@gmail.com
+ * Author: Hassan Hafeez
+ * Author URI: mailto:iamhassanhafeez@gmail.com
+ * Version: 1.0.0
+ * License: GPL2
+ */
+
+
+?>
